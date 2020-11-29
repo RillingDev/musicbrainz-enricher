81cff1f374daf9643811474ea64e57971c6d845f
FelixRilling <contact@rilling.dev>
2020-11-29 14:52:57 +0100

    Use filter for wikidata requests.

--- src/main/java/org/felixrilling/musicbrainzenricher/api/wikidata/WikidataService.java
+++ src/main/java/org/felixrilling/musicbrainzenricher/api/wikidata/WikidataService.java
@@ -1,26 +1,34 @@
 package org.felixrilling.musicbrainzenricher.api.wikidata;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
-import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
-import org.wikidata.wdtk.datamodel.interfaces.Statement;
-import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
+import org.wikidata.wdtk.datamodel.helpers.Datamodel;
+import org.wikidata.wdtk.datamodel.interfaces.*;
+import org.wikidata.wdtk.wikibaseapi.BasicApiConnection;
 import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
 import org.wikidata.wdtk.wikibaseapi.apierrors.MediaWikiApiErrorException;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class WikidataService {
     private static final Logger logger = LoggerFactory.getLogger(WikidataService.class);
 
-    private final WikibaseDataFetcher fetcher = WikibaseDataFetcher.getWikidataDataFetcher();
+    private final BasicApiConnection wikidataApiConnection = BasicApiConnection.getWikidataApiConnection();
 
-    public Optional<List<Statement>> findEntityPropertyValues(String entityId, String propertyId) {
+    public @NotNull Optional<List<Statement>> findEntityPropertyValues(@NotNull String entityId, @NotNull String propertyId) {
+        PropertyIdValue propertyIdValue = Datamodel.makeWikidataPropertyIdValue(propertyId);
+
+        WikibaseDataFetcher fetcher = new WikibaseDataFetcher(wikidataApiConnection, Datamodel.SITE_WIKIDATA);
+        fetcher.getFilter().excludeAllLanguages();
+        fetcher.getFilter().excludeAllSiteLinks();
+        fetcher.getFilter().setPropertyFilter(Set.of(propertyIdValue));
+
         EntityDocument entityDocument;
         try {
             entityDocument = fetcher.getEntityDocument(entityId);
@@ -33,7 +41,7 @@ public class WikidataService {
             return Optional.empty();
         }
 
-        StatementGroup statementGroup = ((ItemDocument) entityDocument).findStatementGroup(propertyId);
+        StatementGroup statementGroup = ((ItemDocument) entityDocument).findStatementGroup(propertyIdValue);
         if (statementGroup == null) {
             return Optional.empty();
         }
