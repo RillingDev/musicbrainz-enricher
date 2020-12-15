beaf6cb5c95b54238dbdeb7b4ec73633be87b176
FelixRilling <contact@rilling.dev>
2020-12-15 15:14:26 +0100

    Remove unused base discogs model, use JsonCreator.

--- src/main/java/org/felixrilling/musicbrainzenricher/api/discogs/DiscogsMaster.java
+++ src/main/java/org/felixrilling/musicbrainzenricher/api/discogs/DiscogsMaster.java
@@ -1,29 +1,33 @@
 package org.felixrilling.musicbrainzenricher.api.discogs;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Set;
 
 /**
  * (Incomplete) Discogs master.
  */
 // See e.g. https://api.discogs.com/masters/1640131
-public class DiscogsMaster extends DiscogsBaseModel {
-    private Set<String> genres;
-    private Set<String> styles;
+public class DiscogsMaster {
+    private final Set<String> genres;
+    private final Set<String> styles;
+
+    @JsonCreator
+    public DiscogsMaster(
+            @JsonProperty(value = "genres", required = true) Set<String> genres,
+            @JsonProperty(value = "styles", required = true) Set<String> styles
+    ) {
+        this.genres = genres;
+        this.styles = styles;
+    }
 
     public Set<String> getGenres() {
         return genres;
     }
 
-    public void setGenres(Set<String> genres) {
-        this.genres = genres;
-    }
-
     public Set<String> getStyles() {
         return styles;
-    }
-
-    public void setStyles(Set<String> styles) {
-        this.styles = styles;
     }
 }
