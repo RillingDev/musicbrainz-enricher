21f001ab5de70024e78352ded96e83dc218090a0
FelixRilling <contact@rilling.dev>
2021-08-28 10:12:57 +0200

    Create history schema in pgsql.

--- src/main/resources/db/migration/V1_0__create_history_schema.sql
+++ src/main/resources/db/migration/V1_0__create_history_schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE history_entry
 (
-    id           BIGINT                   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id           BIGINT                   NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     mbid         UUID                     NOT NULL,
     data_type    INTEGER                  NOT NULL,
     last_checked TIMESTAMP WITH TIME ZONE NOT NULL
 );
-CREATE UNIQUE INDEX history_entry_unique_index_c ON history_entry (mbid, data_type);
+CREATE UNIQUE INDEX history_entry_unique_index ON history_entry (mbid, data_type);
