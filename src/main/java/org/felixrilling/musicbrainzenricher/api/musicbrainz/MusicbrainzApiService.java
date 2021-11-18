330f1f2c9d82507416e157f52e62bc736086bbc0
FelixRilling <contact@rilling.dev>
2021-11-18 19:55:51 +0100

    Fixed client name.

--- src/main/java/org/felixrilling/musicbrainzenricher/api/musicbrainz/MusicbrainzApiService.java
+++ src/main/java/org/felixrilling/musicbrainzenricher/api/musicbrainz/MusicbrainzApiService.java
@@ -28,19 +28,18 @@ class MusicbrainzApiService {
 	private String password;
 
 	@NotNull WebService createWebService() {
-		HttpClientWebServiceWs2 webService = new HttpClientWebServiceWs2();
-		webService.setClient(getClientName(applicationName, applicationVersion, applicationContact));
+		HttpClientWebServiceWs2 webService = new HttpClientWebServiceWs2(applicationName,
+			applicationVersion,
+			applicationContact);
+		webService.setClient(getClientName(applicationName, applicationVersion));
 		webService.setUsername(username);
 		webService.setPassword(password);
 		webService.setHost(host);
 		return webService;
 	}
 
-	private static @NotNull String getClientName(@NotNull String applicationName,
-												 @NotNull String applicationVersion,
-												 @NotNull String applicationContact) {
-		// See https://musicbrainz.org/doc/MusicBrainz_API/Rate_Limiting
-		// While Musicbrainz states that contact details should be added, these seem to cause issues with the Java API.
-		return String.format("%s/%s", applicationName, applicationVersion);
+	private static @NotNull String getClientName(@NotNull String applicationName, @NotNull String applicationVersion) {
+		// See org.musicbrainz.webservice.DefaultWebServiceWs2.client
+		return String.format("%s-%s", applicationName, applicationVersion);
 	}
 }
