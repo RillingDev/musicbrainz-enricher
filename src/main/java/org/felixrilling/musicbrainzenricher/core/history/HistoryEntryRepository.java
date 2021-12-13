0a4970443b7735ff5d8c778c72e747543fd2a4c3
FelixRilling <contact@rilling.dev>
2021-12-13 08:24:39 +0100

    Unify data_type/mbid order.

--- src/main/java/org/felixrilling/musicbrainzenricher/core/history/HistoryEntryRepository.java
+++ src/main/java/org/felixrilling/musicbrainzenricher/core/history/HistoryEntryRepository.java
@@ -26,21 +26,21 @@ class HistoryEntryRepository {
 						   @NotNull ZonedDateTime consideredActiveIfAfter) {
 		Long count = Objects.requireNonNull(jdbcTemplate.queryForObject("""
 			SELECT COUNT(*) FROM musicbrainz_enricher.history_entry he
-				WHERE he.mbid = ?
-				AND he.data_type = ?
+				WHERE he.data_type = ?
+				AND he.mbid = ?
 				AND he.last_checked > ?
-			""", Long.class, mbid, serializeDataType(dataType), serializeZonedDateTime(consideredActiveIfAfter)));
+			""", Long.class, serializeDataType(dataType), mbid, serializeZonedDateTime(consideredActiveIfAfter)));
 
 		return count > 0;
 	}
 
 	void persist(@NotNull HistoryEntry historyEntry) {
 		jdbcTemplate.update("""
-				INSERT INTO musicbrainz_enricher.history_entry (mbid, data_type, last_checked) VALUES (?, ?, ?)
-				ON CONFLICT (mbid, data_type) DO UPDATE SET last_checked = excluded.last_checked
+				INSERT INTO musicbrainz_enricher.history_entry (data_type, mbid, last_checked) VALUES (?, ?, ?)
+				ON CONFLICT (data_type, mbid) DO UPDATE SET last_checked = excluded.last_checked
 				""",
-			historyEntry.mbid(),
 			serializeDataType(historyEntry.dataType()),
+			historyEntry.mbid(),
 			serializeZonedDateTime(historyEntry.lastChecked()));
 	}
 
