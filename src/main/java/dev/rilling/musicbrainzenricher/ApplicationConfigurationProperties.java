7ce3ff0cc3a8988290b1dabd4127dad6f012f756
FelixRilling <contact@rilling.dev>
2022-12-16 17:56:58 +0100

    Updated deps.

--- src/main/java/dev/rilling/musicbrainzenricher/ApplicationConfigurationProperties.java
+++ src/main/java/dev/rilling/musicbrainzenricher/ApplicationConfigurationProperties.java
@@ -1,11 +1,10 @@
 package dev.rilling.musicbrainzenricher;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Configuration
 @ConfigurationProperties(prefix = "musicbrainz-enricher")
