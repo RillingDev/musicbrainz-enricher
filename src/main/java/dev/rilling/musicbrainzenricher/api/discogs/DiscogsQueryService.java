d416e8eb5b45f711a7b9b424da7b71ce49bd1ab6
FelixRilling <contact@rilling.dev>
2023-12-15 17:32:54 +0100

    Use RestClient instead of RestTemplate.

--- src/main/java/dev/rilling/musicbrainzenricher/api/discogs/DiscogsQueryService.java
+++ src/main/java/dev/rilling/musicbrainzenricher/api/discogs/DiscogsQueryService.java
@@ -2,17 +2,15 @@ package dev.rilling.musicbrainzenricher.api.discogs;
 
 import dev.rilling.musicbrainzenricher.api.BucketService;
 import net.jcip.annotations.ThreadSafe;
-import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 import java.util.Optional;
@@ -26,23 +24,15 @@ public class DiscogsQueryService {
 
 	private final DiscogsBucketProvider discogsBucketProvider;
 	private final BucketService bucketService;
-	private final RestTemplateBuilder restTemplateBuilder;
-
-	private final RestTemplate webClient;
+	private final RestClient restClient;
 
 	DiscogsQueryService(Environment environment,
 						DiscogsBucketProvider discogsBucketProvider,
-						BucketService bucketService,
-						RestTemplateBuilder restTemplateBuilder) {
+						BucketService bucketService) {
 		this.discogsBucketProvider = discogsBucketProvider;
 		this.bucketService = bucketService;
-		this.restTemplateBuilder = restTemplateBuilder;
 
-		String applicationName = environment.getRequiredProperty("musicbrainz-enricher.name");
-		String applicationVersion = environment.getRequiredProperty("musicbrainz-enricher.version");
-		String applicationContact = environment.getRequiredProperty("musicbrainz-enricher.contact");
-		String token = environment.getProperty("musicbrainz-enricher.discogs.token");
-		webClient = createWebClient(applicationName, applicationVersion, applicationContact, token);
+		restClient = createRestClient(environment);
 	}
 
 	@NotNull
@@ -50,9 +40,7 @@ public class DiscogsQueryService {
 		bucketService.consumeSingleBlocking(discogsBucketProvider.getBucket());
 
 		try {
-			return Optional.ofNullable(webClient.getForObject("/releases/{id}",
-				DiscogsRelease.class,
-				Map.of("id", id)));
+			return Optional.ofNullable(restClient.get().uri("/releases/{id}", Map.of("id", id)).accept().retrieve().body(DiscogsRelease.class));
 		} catch (RestClientException e) {
 			LOGGER.warn("Could not look up release '{}'.", id, e);
 			return Optional.empty();
@@ -64,7 +52,7 @@ public class DiscogsQueryService {
 		bucketService.consumeSingleBlocking(discogsBucketProvider.getBucket());
 
 		try {
-			return Optional.ofNullable(webClient.getForObject("/masters/{id}", DiscogsMaster.class, Map.of("id", id)));
+			return Optional.ofNullable(restClient.get().uri("/masters/{id}", Map.of("id", id)).retrieve().body(DiscogsMaster.class));
 		} catch (RestClientException e) {
 			LOGGER.warn("Could not look up master '{}'.", id, e);
 			return Optional.empty();
@@ -72,27 +60,24 @@ public class DiscogsQueryService {
 	}
 
 	@NotNull
-	private RestTemplate createWebClient(@NotNull String applicationName,
-										 @NotNull String applicationVersion,
-										 @NotNull String applicationContact,
-										 String token) {
-		String userAgent = getUserAgent(applicationName, applicationVersion, applicationContact);
+	private RestClient createRestClient(Environment environment) {
+		String applicationName = environment.getRequiredProperty("musicbrainz-enricher.name");
+		String applicationVersion = environment.getRequiredProperty("musicbrainz-enricher.version");
+		String applicationContact = environment.getRequiredProperty("musicbrainz-enricher.contact");
+		// See https://www.discogs.com/developers/
+		String userAgent = "%s/%s +%s".formatted(applicationName, applicationVersion, applicationContact);
 
-		RestTemplateBuilder builder = restTemplateBuilder.rootUri("https://api.discogs.com")
+		RestClient.Builder builder = RestClient.builder()
+			.baseUrl("https://api.discogs.com")
 			.defaultHeader(HttpHeaders.USER_AGENT, userAgent)
 			.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-		if (!StringUtils.isEmpty(token)) {
-			// https://www.discogs.com/developers/#page:authentication
-			builder = builder.defaultHeader(HttpHeaders.AUTHORIZATION, "Discogs token=%s".formatted(token));
-		}
-		return builder.build();
-	}
 
-	private @NotNull String getUserAgent(@NotNull String applicationName,
-										 @NotNull String applicationVersion,
-										 @NotNull String applicationContact) {
-		// See https://www.discogs.com/developers/
-		return "%s/%s +%s".formatted(applicationName, applicationVersion, applicationContact);
+		if (environment.containsProperty("musicbrainz-enricher.discogs.token")) {
+			// https://www.discogs.com/developers/#page:authentication
+			builder = builder.defaultHeader(HttpHeaders.AUTHORIZATION, "Discogs token=%s".formatted(environment.getProperty("musicbrainz-enricher.discogs.token")));
+		}
+
+		return builder.build();
 	}
 
 }
