a3e92a9972a23835a44465dd2d606fd8ae1c913f
FelixRilling <contact@rilling.dev>
2023-12-26 11:08:50 +0100

    Fixed nullity.

--- src/main/java/dev/rilling/musicbrainzenricher/api/discogs/DiscogsMaster.java
+++ src/main/java/dev/rilling/musicbrainzenricher/api/discogs/DiscogsMaster.java
@@ -3,6 +3,7 @@ package dev.rilling.musicbrainzenricher.api.discogs;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.annotation.Nullable;
 
 import java.util.Set;
 
@@ -10,10 +11,10 @@ import java.util.Set;
  * (Incomplete) Discogs master.
  */
 // See e.g. https://api.discogs.com/masters/1640131
-public record DiscogsMaster( Set<String> genres, Set<String> styles) {
+public record DiscogsMaster(Set<String> genres, @Nullable Set<String> styles) {
 	@JsonCreator
 	public DiscogsMaster(@JsonProperty(value = "genres", required = true) Set<String> genres,
-						 @JsonProperty(value = "styles") Set<String> styles) {
+						 @JsonProperty(value = "styles") @Nullable Set<String> styles) {
 		this.genres = Set.copyOf(genres);
 		this.styles = styles != null ? Set.copyOf(styles) : null;
 	}
--- src/main/java/dev/rilling/musicbrainzenricher/api/discogs/DiscogsRelease.java
+++ src/main/java/dev/rilling/musicbrainzenricher/api/discogs/DiscogsRelease.java
@@ -3,6 +3,7 @@ package dev.rilling.musicbrainzenricher.api.discogs;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.annotation.Nullable;
 
 import java.util.Set;
 
@@ -10,10 +11,10 @@ import java.util.Set;
  * (Incomplete) Discogs release.
  */
 // See e.g. https://api.discogs.com/releases/249504
-public record DiscogsRelease( Set<String> genres, Set<String> styles) {
+public record DiscogsRelease(Set<String> genres, @Nullable Set<String> styles) {
 	@JsonCreator
 	public DiscogsRelease(@JsonProperty(value = "genres", required = true) Set<String> genres,
-						  @JsonProperty(value = "styles") Set<String> styles) {
+						  @JsonProperty(value = "styles") @Nullable Set<String> styles) {
 		this.genres = Set.copyOf(genres);
 		this.styles = styles != null ? Set.copyOf(styles) : null;
 	}
