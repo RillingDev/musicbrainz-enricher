5de2d1a5d13839a6d680d96fe89ab953d5f2fe40
RillingDev <contact@rilling.dev>
2025-09-25 17:53:07 +0200

    Soft-reference release group in schema.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -45,7 +45,8 @@ WHERE rghe.release_group_gid IS NULL;
 
 CREATE TABLE IF NOT EXISTS musicbrainz_enricher.enricher_release_group_result
 (
-	target_release_group_gid uuid    NOT NULL REFERENCES musicbrainz.release_group (gid) ON DELETE CASCADE,
+	-- Until we switch to using lookups against the DB instead of the API, this is not hard reference
+	target_release_group_gid uuid NOT NULL /*REFERENCES musicbrainz.release_group (gid) ON DELETE CASCADE*/,
 	-- This implicitly references musicbrainz.url. As its only used for debugging, no constraint is used.
 	source_url               VARCHAR NOT NULL,
 	-- This implicitly references musicbrainz.genre. To make submission easier it is denormalized here
